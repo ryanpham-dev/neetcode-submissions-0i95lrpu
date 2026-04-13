a8b62e9447fe501ad2cf1f0e62189cf5c92577eb
ryanpham-dev <152468915+ryanpham-dev@users.noreply.github.com>
2026-04-13 01:56:11 -0500

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.cpp
@@ -0,0 +1,37 @@
+class LRUCache {
+    std::list<std::pair<int, int>> cacheNodes;
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> cacheMap;
+    int capacity;
+
+public:
+    explicit LRUCache(int cap) : capacity(cap) { cacheMap.reserve(cap); }
+
+    int get(int key) {
+        int output = -1;
+        if (cacheMap.find(key) != cacheMap.end()) {
+            auto cacheIter = cacheMap.at(key);
+            output = cacheIter->second;
+            cacheNodes.erase(cacheIter);
+            cacheNodes.emplace_front(key, output);
+            cacheMap[key] = cacheNodes.begin();
+        }
+        return output;
+    }
+
+    void put(int key, int value) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            auto cacheIter = cacheMap.at(key);
+            cacheNodes.erase(cacheIter);
+            cacheNodes.emplace_front(key, value);
+            cacheMap[key] = cacheNodes.begin();
+        } else {
+            cacheNodes.emplace_front(key, value);
+            cacheMap.emplace(key, cacheNodes.begin());
+            if (static_cast<int>(cacheMap.size()) > capacity) {
+                auto [leastUsedKey, leastUsedValue] = cacheNodes.back();
+                cacheMap.erase(leastUsedKey);
+                cacheNodes.pop_back();
+            }
+        }
+    }
+};
