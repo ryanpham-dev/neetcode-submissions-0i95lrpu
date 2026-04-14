23813284097f09f9f083762e5dc669140fcf1416
ryanpham-dev <152468915+ryanpham-dev@users.noreply.github.com>
2026-04-13 22:00:07 -0500

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,19 @@
+class TimeMap {
+    std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> store{};
+
+public:
+    TimeMap() = default;
+    void set(std::string key, std::string value, int timestamp) {
+        store[key].emplace_back(timestamp, value);
+    }
+    std::string get(std::string key, int timestamp) {
+        auto times = store[key];
+        auto iter{
+            std::upper_bound(times.begin(), times.end(), timestamp,
+                             [](auto target, auto& element) { return target < element.first; })};
+        if (iter != times.begin()) {
+            return std::prev(iter)->second;
+        }
+        return {};
+    }
+};
