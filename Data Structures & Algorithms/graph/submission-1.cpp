83b1165ea16aa8c7744ab15f091b9f89cfc10b6f
ryanpham-dev <152468915+ryanpham-dev@users.noreply.github.com>
2026-04-22 21:31:44 -0500

    Add: graph - submission-1

---
+++ Data Structures & Algorithms/graph/submission-1.cpp
@@ -0,0 +1,43 @@
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+class Graph {
+    std::unordered_map<int, std::unordered_set<int>> graph{};
+
+public:
+    Graph() = default;
+    void addEdge(int src, int dst) {
+        graph[src].insert(dst);
+        graph[dst];
+    }
+
+    bool removeEdge(int src, int dst) { return static_cast<bool>(graph[src].erase(dst)); }
+
+    bool hasPath(int src, int dst) {
+        if (src == dst) {
+            return true;
+        }
+
+        std::unordered_set<int> visited{};
+        std::queue<int> frontier{};
+        frontier.push(src);
+        visited.insert(src);
+        while (!frontier.empty()) {
+            auto size = frontier.size();
+            for (auto i = 0ul; i < size; ++i) {
+                int current = frontier.front();
+                frontier.pop();
+                if (current == dst) {
+                    return true;
+                }
+                for (const int neighbor : graph.at(current)) {
+                    if (!visited.contains(neighbor)) {
+                        visited.insert(neighbor);
+                        frontier.push(neighbor);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
